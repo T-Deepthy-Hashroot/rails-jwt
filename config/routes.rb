7851f9ad88a4bc5bd5a5afd46be376fa0d7114ee
T-Deepthy <deeps1929@gmail.com>
2019-09-30 18:07:06 +0530

    Remove resources: :tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
-  resources: :tasks
   post '/auth/login', to: 'authentication#login'
   # get '/*a', to: 'application#not_found'
   resources :users do
