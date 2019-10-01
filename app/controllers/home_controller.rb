f6a02bfb1322b27e1b798217ae0034b4a5241809
T-Deepthy <deeps1929@gmail.com>
2019-10-01 12:25:07 +0530

    Add show action in Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authorize_request
   def new
+    render html: "Helllo"
+  end
+
+  def show
     render html: "Email is confirmed and account is activated!"
   end
 end
